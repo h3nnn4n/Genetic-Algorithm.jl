538e3aafa5333414a92f156acc980822c917b540
h3nnn4n <uber.renan@gmail.com>
2016-06-10 17:56:32 -0300

    trying to pmap

--- src/tester.jl
+++ src/tester.jl
@@ -4,15 +4,17 @@ Base.(:+)(x :: Tuple{Int64,Float64,Bool}, y :: Tuple{Int64,Float64,Bool}) = (x[1
 Base.(:+)(x :: Tuple{Int64,Float64,Int64}, y :: Tuple{Int64,Float64,Bool}) = (x[1] + y[1], x[2] + y[2], x[3] + y[3])
 
 function tester()
-    n     = nworkers()
-    out   = open("data.log", "w")
+    n   = nworkers()
+    t   = 10
+    out = open("data.log", "w")
     cd("../instances")
     files = readdir()
     for file in files
         name = split(splitext(file)[1], '-')[1]
         if name == "uf20"
             println("$file \t")
-            info = sum([main(file) for i in 1:10])
+            todo = [file for i in 1:t]
+            info = pmap(x -> main(x), todo)
             println(info)
             write(out, "$file $info\n")
             flush(out)
