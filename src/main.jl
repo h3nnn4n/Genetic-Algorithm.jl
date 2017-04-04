abd5e9bd33f3e07610cd9cd34b359ea5b6e361ec
h3nnn4n <uber.renan@gmail.com>
2017-04-04 13:31:25 -0300

    Added the alternating bit

--- src/main.jl
+++ src/main.jl
@@ -1,7 +1,18 @@
 include("types.jl")
 include("population.jl")
 
-function fitness( ind :: _individual )
+function fitness_alternating_bit( ind :: _individual )
+    fit = 0 :: Int64
+    for i in 2:ind.n_genes
+        if ind.genetic_code[i].value $ ind.genetic_code[i - 1].value
+            fit += 1
+        end
+    end
+
+    ind.fitness = fit
+end
+
+function fitness_alternating_parity( ind :: _individual )
     fit = 0 :: Int64
     for i in 2:ind.n_genes
         if (ind.genetic_code[i].value % 2) != (ind.genetic_code[i - 1].value % 2)
@@ -14,17 +25,17 @@ end
 
 function main()
     pop = spawn_empty_population()
-    pop.size = 10
-    pop.n_genes = 5
+    pop.size = 30
+    pop.n_genes = 50
     pop.mchance = 0.05
     pop.cchance = 0.75
 
     for i in 1:pop.size
         new_guy = _individual(pop.n_genes, 0, [])
         for j in 1:pop.n_genes
-            #=new_gene = _gene(true, false, false, false, false, true, false)=#
+            new_gene = _gene(true, false, false, false, false, true, false)
             #=new_gene = _gene(false, true, false, false, 0.0, 1.0, 0.0)=#
-            new_gene = _gene(false, false, true, false, 0, 10, 0)
+            #=new_gene = _gene(false, false, true, false, 0, 10, 0)=#
             #=new_gene = _gene(false, false, false, true, 0, 10, 0)=#
             push!(new_guy.genetic_code, new_gene)
         end
@@ -33,16 +44,24 @@ function main()
 
     init_population(pop)
 
-    for iter in 1:5
-        evaluate(pop, fitness)
+    for iter in 1:100
+        #=evaluate(pop, fitness_alternating_parity)=#
+        evaluate(pop, fitness_alternating_bit)
 
-        print_pop(pop)
-        println()
+        #=print_pop(pop)=#
+        #=println()=#
+
+        if iter % 10 == 0
+            println("$iter")
+        end
 
         selection(pop)
         crossover(pop)
         mutation(pop)
     end
+
+    print_pop(pop)
+    println()
 
     return
 end
